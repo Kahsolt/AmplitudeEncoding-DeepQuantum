03564357d7d9ab0ae8378b2d8c5633cfff3b7886
Kahsolt <kahsolt@qq.com>
2024-09-04 19:06:48 +0800

    amend vis_sample_cntsum

--- p1/vis_sample_cntsum.py
+++ p1/vis_sample_cntsum.py
@@ -11,26 +11,47 @@ from utils import *
 
 datatset = QMNISTDatasetIdea(label_list=[0,1,2,3,4], train=False)
 cnt = torch.zeros([1, 28, 28], dtype=torch.int32)
-for x, _, _ in tqdm(datatset):
-  cnt += (denormalize(x) > 1/255).to(torch.int32)
+avg = torch.zeros([1, 28, 28], dtype=torch.float32)
+for x, y, z_func in tqdm(datatset):
+  x = denormalize(x).clamp(0.0, 1.0)
+  cnt += (x > 1/255).to(torch.int32)
+  avg +=  x         .to(torch.float32)
+avg = (avg / len(datatset) * 255).ceil().to(torch.uint8)
+avg[avg==1] = 0   # ignore small value
 
-Z = cnt[0].numpy()
-sns.heatmap(Z, cmap='Reds', fmt='d', annot=True)
-plt.suptitle('vis_sample_cntsum')
-#plt.show()
+map_cnt = cnt[0].numpy()
+map_avg = avg[0].numpy()
 
-if 'save stats':
-  cnt_loc = []
-  for i in range(Z.shape[0]):
-    for j in range(Z.shape[1]):
-      cnt_loc.append((Z[i, j], (i, j)))
+plt.clf()
+plt.figure(figsize=(14, 10))
+sns.heatmap(map_cnt, cmap='Reds', fmt='d', annot=True)
+plt.suptitle('vis_sample_cnt')
+plt.tight_layout()
+plt.savefig('./output/vis_sample_cnt.png', dpi=400)
 
-  def cmp(it):
-    # cnt 越大, (i,j) 越靠近中心
-    cnt, (i, j) = it
-    return (-cnt, np.sqrt((i-14)**2 + (j-14)**2))
-  cnt_loc.sort(key=cmp)
-  print(cnt_loc)
+plt.clf()
+plt.figure(figsize=(14, 10))
+sns.heatmap(map_avg, cmap='Reds', fmt='d', annot=True)
+plt.suptitle('vis_sample_avg')
+plt.tight_layout()
+plt.savefig('./output/vis_sample_avg.png', dpi=400)
 
-  loc_list = [loc for cnt, loc in cnt_loc]   # [(x, y)]
-  np.save('./output/loc.npy', np.asarray(loc_list, dtype=np.uint8))
+avg_empty = (avg == 0).sum()
+# >> empty: 313 (ratio: 0.39923468232154846)
+print(f'>> empty: {avg_empty} (ratio: {avg_empty / avg.numel()})')
+
+Z = map_avg    # 以值的avg为重要性
+cnt_loc = []
+for i in range(Z.shape[0]):
+  for j in range(Z.shape[1]):
+    cnt_loc.append((Z[i, j], (i, j)))
+
+def cmp(it):  # 优先: cnt 越大, (i,j) 越靠近中心
+  cnt, (i, j) = it
+  return (-cnt, np.sqrt((i-14)**2 + (j-14)**2))
+cnt_loc.sort(key=cmp)
+
+loc_list = [loc for cnt, loc in cnt_loc if cnt]   # [(x, y)]
+# len(loc_list): 471
+print('len(loc_list):', len(loc_list))
+np.save('./output/loc.npy', np.asarray(loc_list, dtype=np.uint8))
